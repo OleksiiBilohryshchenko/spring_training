54502321ec7ebb8d3a66a9422d8a2dcf1d1e1032
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-03-20 19:30:00 -0700

    Coupling

--- spring-00-Coupling/src/main/java/com/example/loosely_coupled/PC.java
+++ spring-00-Coupling/src/main/java/com/example/loosely_coupled/PC.java
@@ -1,4 +1,27 @@
 package com.example.loosely_coupled;
 
+import com.example.loosely_coupled.casefactory.Case;
+import com.example.loosely_coupled.monitorfactory.Monitor;
+import com.example.loosely_coupled.motherboardfactory.Motherboard;
+
 public class PC {
+    private Case theCase;
+    private Monitor monitor;
+    private Motherboard motherboard;
+
+    public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
+        this.theCase = theCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+    public void powerUp() {
+        theCase.pressPowerButton();
+        drawLogo();
+    }
+
+    private void drawLogo() {
+        monitor.drawPixelAt();
+    }
 }
+
