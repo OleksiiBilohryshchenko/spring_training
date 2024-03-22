c22e6ca5a4455b402df8644a4872b454ef653d62
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-03-21 22:21:38 -0600

    Autowiring

--- spring-04-Autowiring/src/main/java/com/example/stereotype_annotation/PC.java
+++ spring-04-Autowiring/src/main/java/com/example/stereotype_annotation/PC.java
@@ -5,14 +5,19 @@ import com.example.stereotype_annotation.casefactory.Case;
 import com.example.stereotype_annotation.monitorfactory.Monitor;
 import com.example.stereotype_annotation.motherboardfactory.Motherboard;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Getter
+@Component
 public class PC {
+
 
     private Case theCase;
     private Monitor monitor;
     private Motherboard motherboard;
 
+    @Autowired // constructor injection, but it is not needed if only 1 constructor or just use @AllArgsConstructor
     public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
         this.theCase = theCase;
         this.monitor = monitor;
