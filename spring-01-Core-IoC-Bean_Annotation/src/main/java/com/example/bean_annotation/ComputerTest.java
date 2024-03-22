a4d2665063277c7289ae48ded589b64145392958
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-03-21 19:37:44 -0600

    Core-IoC-Config

--- spring-01-Core-IoC-Bean_Annotation/src/main/java/com/example/bean_annotation/ComputerTest.java
+++ spring-01-Core-IoC-Bean_Annotation/src/main/java/com/example/bean_annotation/ComputerTest.java
@@ -46,11 +46,14 @@ public class ComputerTest {
 
         System.out.println("************Multiple Objects******************");
 
-        Monitor theMonitor2 = container.getBean("monitorSony", Monitor.class);  //DEFAULT BEAN NAME
-        Monitor theMonitor3 = container.getBean("sony", Monitor.class);  //CUSTOM BEAN NAME
+//        Monitor theMonitor2 = container.getBean("monitorSony", Monitor.class);  //DEFAULT BEAN NAME
+//        Monitor theMonitor3 = container.getBean("sony", Monitor.class);  //CUSTOM BEAN NAME
         Monitor theMonitor4 = container.getBean( Monitor.class);  //@Primary
 
+        Case theCase = container.getBean(Case.class);
+        Motherboard theMotherboard = container.getBean(Motherboard.class);
 
+        PC myPc2 = new PC(theCase,theMonitor4,theMotherboard);
 
 
 
