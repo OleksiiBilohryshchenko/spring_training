faa0f24eaf564da44b030c4094bafdfe49a95154
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-03-21 20:12:22 -0600

    Core-IoC-Stereotype

--- spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/example/stereotype_annotation/monitorfactory/AcerMonitor.java
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/example/stereotype_annotation/monitorfactory/AcerMonitor.java
@@ -2,18 +2,19 @@ package com.example.stereotype_annotation.monitorfactory;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
+@Component
 public class AcerMonitor extends Monitor {
 
-    public AcerMonitor(String model, String manufacturer, int size) {
-        super(model, manufacturer, size);
+    public AcerMonitor() {
+
+        super("25 inch beast","Acer",25);
     }
 
     public void drawPixelAt() {
-        System.out.println("Drawing pixel with Acer");
+        System.out.println("Drawing pixel with Sony");
     }
-
-
 }
