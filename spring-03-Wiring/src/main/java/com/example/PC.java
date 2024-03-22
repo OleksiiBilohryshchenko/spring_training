d34aefb605bfad25814e7fecd9e7d163b3454446
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-03-21 21:16:04 -0600

    Core-IoC-Wiring

---
+++ spring-03-Wiring/src/main/java/com/example/PC.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import com.example.casefactory.Case;
+import com.example.monitorfactory.Monitor;
+import com.example.motherboardfactory.Motherboard;
+import lombok.Getter;
+
+@Getter
+public class PC {
+
+    private Case theCase;
+    private Monitor monitor;
+    private Motherboard motherboard;
+
+    public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
+        this.theCase = theCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+    public void powerUp() {
+        theCase.pressPowerButton();
+        drawLogo();
+    }
+
+    private void drawLogo() {
+        // Fancy graphics
+        monitor.drawPixelAt();
+    }
+
+}
+
