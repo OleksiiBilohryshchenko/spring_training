f0052bf72fc633b702427d8a74bb9a16d98c2cd6
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-03-21 19:59:22 -0600

    Core-IoC-Stereotype

---
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/example/stereotype_annotation/config/PcConfig.java
@@ -0,0 +1,12 @@
+package com.example.stereotype_annotation.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.example.stereotype_annotation")  // basically check where to search
+public class PcConfig {
+
+
+
+}
