70074174e5ca2ee47316aad36dfbaa0003ad7864
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-03-21 19:43:31 -0600

    Core-IoC-Stereotype

---
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/example/stereotype_annotation/monitorfactory/SonyMonitor.java
@@ -0,0 +1,20 @@
+package com.example.stereotype_annotation.monitorfactory;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+public class SonyMonitor extends Monitor {
+
+    public SonyMonitor(String model, String manufacturer, int size) {
+
+        super(model, manufacturer, size);
+    }
+
+    public void drawPixelAt() {
+        System.out.println("Drawing pixel with Sony");
+    }
+}
