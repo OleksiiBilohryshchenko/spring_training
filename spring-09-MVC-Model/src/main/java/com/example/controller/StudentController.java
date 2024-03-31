832288cabb303c80bfbe8d0b6a895bb14ec67c8e
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-03-31 16:12:46 -0500

    MVC Model

--- spring-09-MVC-Model/src/main/java/com/example/controller/StudentController.java
+++ spring-09-MVC-Model/src/main/java/com/example/controller/StudentController.java
@@ -1,13 +1,16 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class StudentController {
-    @RequestMapping
-    public String homePage(){
-        return ""
+    @RequestMapping("/welcome")
+    public String homePage(Model model){
+        model.addAttribute("name","Alex");
+        model.addAttribute("topic","MVC");
+        return "student/welcome";
     }
 
 }
