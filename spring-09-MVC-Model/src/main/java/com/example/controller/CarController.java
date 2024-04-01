b96f0577b25177ddf01a41fd3ebc07058f96dde9
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-03-31 19:26:32 -0500

    MVC Model

--- spring-09-MVC-Model/src/main/java/com/example/controller/CarController.java
+++ spring-09-MVC-Model/src/main/java/com/example/controller/CarController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CarController {
+    // localhost:8080/info?make=Acura
     @RequestMapping("/info")
     public String carInfo(@RequestParam String make, Model model){
 
@@ -15,10 +16,22 @@ public class CarController {
         return "car/car-info";
     }
 
+    // localhost:8080/info2 (Acura default)
     @RequestMapping("/info2")
     public String carInfo2(@RequestParam(value="make",required = false,defaultValue = "Acura") String make, Model model){
 
         model.addAttribute("make",make);
+
+        return "car/car-info";
+    }
+
+    // localhost:8080/info3?make=Acura&year=2016
+    @RequestMapping("/info3")
+    public String carInfo3(@RequestParam String make, @RequestParam int year, Model model){
+
+
+        model.addAttribute("make",make);
+        model.addAttribute("year",year);
 
         return "car/car-info";
     }
