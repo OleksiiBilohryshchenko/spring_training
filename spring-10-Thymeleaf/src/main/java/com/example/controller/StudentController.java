ba92ca9d71219eabaea3401fcde58ece93afda14
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-03-31 21:35:26 -0500

    Thymeleaf

--- spring-10-Thymeleaf/src/main/java/com/example/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/example/controller/StudentController.java
@@ -4,6 +4,7 @@ import com.example.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/student")
@@ -19,8 +20,9 @@ public class StudentController {
 
     //localhost:8080/student/welcome
     @RequestMapping("/welcome")
-    public String welcome(){
+    public String welcome(@RequestParam int id){
 
+        System.out.println(id);
         return "student/welcome";
     }
 
