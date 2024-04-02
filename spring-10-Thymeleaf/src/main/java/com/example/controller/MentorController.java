d63c7a3cbd20c1c68334502c167d6a2a189b879f
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-04-02 19:38:38 -0400

    Thymeleaf

--- spring-10-Thymeleaf/src/main/java/com/example/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/example/controller/MentorController.java
@@ -5,13 +5,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @RequestMapping("/mentor")
 public class MentorController {
     //localhost:8080/register
     @GetMapping("/register")  //localhost:8080/mentor/register
     public String register(Model model){
+
+        List<String> batchList = Arrays.asList("JD1","JD2","JD3");
+
         model.addAttribute("mentor",new Mentor());
+        model.addAttribute("batchList",batchList);
+
         return "mentor/mentor-register";
     }
 }
