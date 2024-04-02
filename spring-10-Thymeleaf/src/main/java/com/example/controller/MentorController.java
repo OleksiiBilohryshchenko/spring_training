987f16a129aadb91e5fd975596fa6bcdc3d94013
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-04-02 19:50:39 -0400

    Thymeleaf

--- spring-10-Thymeleaf/src/main/java/com/example/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/example/controller/MentorController.java
@@ -4,6 +4,7 @@ import com.example.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -23,4 +24,10 @@ public class MentorController {
 
         return "mentor/mentor-register";
     }
+    @PostMapping("/confirm")
+    public String submitForm(){
+        return "mentor/mentor-confirmation";
+    }
+
+
 }
