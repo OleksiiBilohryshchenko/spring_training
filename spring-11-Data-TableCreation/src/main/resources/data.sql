80021a0911a52b99eb056d14948067ad532b5f8a
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-05-03 21:03:33 -0600

    Spring Data

--- spring-11-Data-TableCreation/src/main/resources/data.sql
+++ spring-11-Data-TableCreation/src/main/resources/data.sql
@@ -1,2 +1,8 @@
 INSERT INTO EMPLOYEES(id,name)
 VALUES (1,'Mike');
+
+INSERT INTO STUDENT(id,first_name,last_name,email)
+VALUES (1,'Mike', 'Smith', 'mike@gmail.com');
+
+INSERT INTO STUDENT(id,first_name,last_name,email)
+VALUES (2,'Tom', 'Johnson', 'tom@gmail.com');
