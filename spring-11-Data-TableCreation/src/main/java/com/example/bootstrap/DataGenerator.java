7934b00f9a25c510d31c3bd8b5de190df5d40b6c
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-05-03 22:11:49 -0600

    Spring Data

--- spring-11-Data-TableCreation/src/main/java/com/example/bootstrap/DataGenerator.java
+++ spring-11-Data-TableCreation/src/main/java/com/example/bootstrap/DataGenerator.java
@@ -1,9 +1,19 @@
 package com.example.bootstrap;
 
 import com.example.entity.Car;
+import com.example.repository.CarRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DataGenerator implements CommandLineRunner {
+
+    private final CarRepository carRepository;
+
+    public DataGenerator(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -11,5 +21,13 @@ public class DataGenerator implements CommandLineRunner {
         Car c2 = new Car("Honda","CRV");
         Car c3 = new Car("Lexus","RX");
 
+
+        carRepository.save(c1);
+        carRepository.save(c2);
+        carRepository.save(c3);
+
+
     }
+
+
 }
