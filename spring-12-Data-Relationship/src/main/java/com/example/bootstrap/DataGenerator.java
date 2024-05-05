b344ebe5f5b4b2dce5515c059576599abb334649
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-05-05 08:55:07 -0500

    Spring Data Relationship

--- spring-12-Data-Relationship/src/main/java/com/example/bootstrap/DataGenerator.java
+++ spring-12-Data-Relationship/src/main/java/com/example/bootstrap/DataGenerator.java
@@ -4,6 +4,7 @@ import com.example.entity.Merchant;
 import com.example.entity.Payment;
 import com.example.entity.PaymentDetail;
 import com.example.enums.Status;
+import com.example.repository.MerchantRepository;
 import com.example.repository.PaymentRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -15,9 +16,11 @@ import java.time.LocalDate;
 public class DataGenerator implements CommandLineRunner {
 
     private final PaymentRepository paymentRepository;
+    private final MerchantRepository merchantRepository;
 
-    public DataGenerator(PaymentRepository paymentRepository) {
+    public DataGenerator(PaymentRepository paymentRepository, MerchantRepository merchantRepository) {
         this.paymentRepository = paymentRepository;
+        this.merchantRepository = merchantRepository;
     }
 
     @Override
@@ -38,8 +41,11 @@ public class DataGenerator implements CommandLineRunner {
         payment1.setMerchant(merchant1);
         payment2.setMerchant(merchant1);
 
+        merchantRepository.save(merchant1);
+
         paymentRepository.save(payment1);
         paymentRepository.save(payment2);
+
 
         System.out.println(paymentRepository.findById(2L).get().getPaymentDetail().getCommissionAmount());
 
