8a0ad6c5258cd9239c7e1ffd76d9a65750198809
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-05-10 21:34:13 -0600

    Spring Data CinemaLab

--- spring-13-Data-CinemaLab/src/main/java/com/example/entity/Movie.java
+++ spring-13-Data-CinemaLab/src/main/java/com/example/entity/Movie.java
@@ -1,11 +1,16 @@
 package com.example.entity;
 
+import com.example.enums.MovieState;
+import com.example.enums.MovieType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
@@ -20,7 +25,11 @@ public class Movie extends BaseEntity{
     private Integer duration;
     @Column(columnDefinition = "text")
     private String summary;
-
+    @Enumerated(EnumType.STRING)
+    private MovieType type;
+    @Enumerated(EnumType.STRING)
+    private MovieState state;
+    private BigDecimal price;
 
 
 }
