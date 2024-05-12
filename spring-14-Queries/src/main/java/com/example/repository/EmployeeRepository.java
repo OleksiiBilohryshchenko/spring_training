1a84ad56f5f72a61b9f57ab8f3b749dbf6293f32
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-05-12 16:36:43 -0700

    Spring Data Queries Derived finished

--- spring-14-Queries/src/main/java/com/example/repository/EmployeeRepository.java
+++ spring-14-Queries/src/main/java/com/example/repository/EmployeeRepository.java
@@ -3,6 +3,7 @@ package com.example.repository;
 import com.example.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
@@ -25,5 +26,17 @@ public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
     //Display all employees with salary less than ""
     List<Employee> findAllBySalaryLessThan(Integer salary);
+
+    //Display all employees that has been hired between "" and ""
+    List<Employee> findAllByHireDateBetween(LocalDate startFate, LocalDate endDate);
+
+    //Display all employees where salary is greater and equal to "" in order-salary
+    List<Employee> findAllBySalaryGreaterThanEqualOrderBySalary(Integer salary);
+
+    //Display top unique 3 employees who are making less than ""
+    List<Employee> findDistinctTop3BySalaryLessThan (Integer salary);
+
+    //Display all employees that do not have email address
+    List<Employee> findAllByEmailIsNull();
 
 }
