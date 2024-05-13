85154844783838d6b6138c66337ece72655e60ea
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-05-12 17:27:45 -0700

    Spring Data Queries JPQL

--- spring-14-Queries/src/main/java/com/example/repository/CourseRepository.java
+++ spring-14-Queries/src/main/java/com/example/repository/CourseRepository.java
@@ -1,9 +1,11 @@
 package com.example.repository;
 
 import com.example.entity.Course;
+import jdk.jfr.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public interface CourseRepository extends JpaRepository <Course,Long>{
 
@@ -13,6 +15,17 @@ public interface CourseRepository extends JpaRepository <Course,Long>{
     //Find all courses by categories and order the entities by name
     List<Course> findByCategoryOrderByName(String category);
 
+    //Check if a course with the supplier exists. Return true if exists, false if not.
+    boolean existsByName(String name);
+
+    //Return a count of courses for given category
+    int countByCategory(String category);
+
+    //Find all the courses that starts with "" given course name string
+    List<Course> findAllByNameStartingWith (String name);
+
+    //Find all courses by category and returns a stream
+    Stream<Course> streamAllByCategory(String category);
 
 
 }
