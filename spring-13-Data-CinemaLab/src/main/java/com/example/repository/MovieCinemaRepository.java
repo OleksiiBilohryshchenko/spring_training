e9604fdc6dda1f03a51901d9a19c81cbd3c46921
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-05-19 15:35:10 -0500

    Spring Data Queries

--- spring-13-Data-CinemaLab/src/main/java/com/example/repository/MovieCinemaRepository.java
+++ spring-13-Data-CinemaLab/src/main/java/com/example/repository/MovieCinemaRepository.java
@@ -49,10 +49,10 @@ public interface MovieCinemaRepository extends JpaRepository<MovieCinema, Long> 
     Integer countByCinemaId(Long cinemaId);
 
     //Write a native query that returns all movie cinemas by location name
-    @Query(value = "SELECT mc.* FROM movie_cinema mc JOIN cinema c ON mc.ciname_id = c.id " +
+    //Write a native query that returns all movie cinemas by location name
+    @Query(value = "SELECT mc.* FROM movie_cinema mc JOIN cinema c ON mc.cinema_id = c.id " +
             " JOIN location l ON c.location_id = l.id " +
-            " WHERE l.name =?1 ",nativeQuery = true)
-    List<MovieCinema> retrieveByLocationName(String name);
-
+            " WHERE l.name = ?1 ",nativeQuery = true)
+    List<MovieCinema> retrieveAllByLocationName(String name);
 
 }
