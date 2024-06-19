48564158dcff30f21add6d0f255d5f97ee0fee0a
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-06-19 18:46:26 -0500

    Spring Rest API

---
+++ spring-15-REST/src/main/java/com/example/controller/CourseController.java
@@ -0,0 +1,28 @@
+package com.example.controller;
+
+import com.example.dto.CourseDTO;
+import com.example.service.CourseService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public List<CourseDTO> getAllCourses(){
+
+        return courseService.getCourses();
+
+    }
+
+}
