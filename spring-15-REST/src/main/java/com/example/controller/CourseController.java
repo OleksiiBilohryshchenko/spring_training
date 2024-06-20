abf7e5b0777deb4dbe3c7e90561961401ca86f7c
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-06-19 19:18:43 -0500

    Spring Rest API

--- spring-15-REST/src/main/java/com/example/controller/CourseController.java
+++ spring-15-REST/src/main/java/com/example/controller/CourseController.java
@@ -36,5 +36,14 @@ public class CourseController {
         return courseService.createCourse(course);
     }
 
+    @PutMapping("{id}")
+    public void updateCourse(@PathVariable("id") long courseId, @RequestBody CourseDTO course){
+        courseService.updateCourse(courseId,course);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteCourseById(@PathVariable("id") long courseId){
+        courseService.deleteCourseById(courseId);
+    }
 
 }
