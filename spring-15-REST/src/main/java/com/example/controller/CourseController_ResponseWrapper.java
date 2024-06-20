c10884b099db90890242b9a41612af7cdb740817
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-06-20 00:13:57 -0500

    Spring Rest API

--- spring-15-REST/src/main/java/com/example/controller/CourseController_ResponseWrapper.java
+++ spring-15-REST/src/main/java/com/example/controller/CourseController_ResponseWrapper.java
@@ -5,6 +5,7 @@ import com.example.service.CourseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +27,9 @@ public class CourseController_ResponseWrapper {
                 .body(new ResponseWrapper("courses successfully retrieved",courseService.getCourses()));
     }
 
+    @GetMapping("{id}")
+    public ResponseEntity<ResponseWrapper> getCourseById(@PathVariable("id") long courseId){
+        return ResponseEntity
+                .ok(new ResponseWrapper("course: " + courseId + " retrieved", courseService.getCourseById(courseId)));
+    }
 }
