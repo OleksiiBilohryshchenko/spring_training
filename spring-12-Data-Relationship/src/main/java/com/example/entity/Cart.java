a25de45baf21358c8956de9267d981427b5620c1
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-06-30 21:32:43 -0500

    Spring Rest

--- spring-12-Data-Relationship/src/main/java/com/example/entity/Cart.java
+++ spring-12-Data-Relationship/src/main/java/com/example/entity/Cart.java
@@ -1,9 +1,9 @@
 package com.example.entity;
 
-import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
---
+++ spring-19-REST-OpenApi3/src/main/java/com/example/service/AccountService.java
@@ -0,0 +1,13 @@
+package com.example.service;
+
+import com.example.dto.AccountDTO;
+
+import java.util.List;
+
+public interface AccountService {
+
+    List<AccountDTO> getAccounts();
+
+    AccountDTO getById(Long id) throws Exception;
+
+}
