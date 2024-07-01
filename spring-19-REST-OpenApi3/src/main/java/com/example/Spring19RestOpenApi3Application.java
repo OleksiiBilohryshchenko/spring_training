a9aca98550b30938ddf8cc13276fcba3e3190128
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-06-30 22:32:27 -0500

    Spring Rest OpenAPI

--- spring-19-REST-OpenApi3/src/main/java/com/example/Spring19RestOpenApi3Application.java
+++ spring-19-REST-OpenApi3/src/main/java/com/example/Spring19RestOpenApi3Application.java
@@ -1,9 +1,14 @@
 package com.example;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Spring19RestOpenApi3Application {
@@ -15,6 +20,15 @@ public class Spring19RestOpenApi3Application {
     @Bean
     public ModelMapper mapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public OpenAPI customOpenApi(){
+        return new OpenAPI().info(new Info().title("Example Application OpenAPI")
+                .version("v2")
+                .description("Example Application API documentation"))
+                .servers(List.of(new Server().url("https://dev.google.com")
+                .description("Dev Environment")));
     }
 
 }
